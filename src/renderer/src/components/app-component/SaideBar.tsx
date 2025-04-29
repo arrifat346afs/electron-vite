551409fea4ae4b3bc6d6e3c8dc3088c605fe8552
Abdur Rahman Rifat <arrifat1235@gmail.com>
2025-04-29 21:07:49 +0600

    fix(sidebar): Adjust scroll area width for better UI

--- src/renderer/src/components/app-component/SaideBar.tsx
+++ src/renderer/src/components/app-component/SaideBar.tsx
@@ -156,7 +156,7 @@ const SaideBar = () => {
               defaultValue={editIndex !== null ? urls[editIndex] : ""}
             />
             {/* vertical scrolling url list */}
-            <ScrollArea className="flex-1 w-[calc(100%-20px)] rounded-md border">
+            <ScrollArea className="flex-1 w-[calc(100%-10px)] rounded-md border">
               <div className="w-[calc(100%-300px)] px-2">
                 {urls.map((url, index) => (
                   <div
